f2f6e005a397001e587325cbce7e16e00fba1d7a
Theodore Carter <Theodore_Carter@student.uml.edu>
2019-10-22 16:12:35 -0400

    Test commit with some real work

---
+++ TelemetryCompanionApp/TelemetryCompanionApp/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TelemetryCompanionApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// This function opens the previous flights window and closes the current window.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void NavToPreviousFlight(object sender, RoutedEventArgs e)
+        {
+            WindowPrevFlight newWin = new WindowPrevFlight();
+            newWin.Show();
+            this.Close();
+        }
+    }
+}
